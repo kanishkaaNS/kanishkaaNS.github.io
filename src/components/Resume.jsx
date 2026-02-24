b0464e6d1bb1f0fedbec457bd40f2bef114b1a54
kanishkaaNS <123634251+kanishkaaNS@users.noreply.github.com>
2026-02-24 18:00:26 +0530

    added my latest resume

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -9,7 +9,7 @@ function Resume(){
                 </p>
 
                 <a
-                href="/Resume03.pdf"
+                href="/Kanishkaa_NS_Frontend_Resume.pdf"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="resume-btn"
